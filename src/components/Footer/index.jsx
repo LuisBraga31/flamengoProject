81ce0d610152f1e86156d8b4778ac65a77fb67c2
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-31 11:22:07 -0300

    Finalizando componente de titulos e adicionando foooter

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,9 @@
+import './footer.css'
+
 export default function Footer() {
     return (
         <footer>
-            
+            <small> © 2023 / Luis Braga Dev - Todos os direitos reservados </small>
         </footer>
     )
 }
